bbb51228001f228a2a774c1d5685d2cd33009bca
Vaibhav-VM-Vidhya <67579239+Vaibhav-VM-Vidhya@users.noreply.github.com>
2022-02-25 16:45:06 -0800

    page chaning testing

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import { useRouter } from 'next/router'
 export default function Home() {
+  const router = useRouter()
+
   return (
     <>
-
-
+   
       <Head>
         <title>Login ▫ Instagram</title>
         <meta name="description" content="Edited and modified by vaibhav And nagesh " />
         <link rel="icon" href="/insta.ico" />
       </Head>
-      <div> Deployment test</div> 
+      <button type="button" onClick={() => router.push('/Login')}>
+      Click me
+    </button> 
       <div className="login">first div</div>
       <div className="signup">Sign up</div>
       <div className="download">Get app</div>
